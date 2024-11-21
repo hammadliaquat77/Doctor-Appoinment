8f2e968d3c0411723e12a5838ba9a85bd1b7ba22
Hammad Liaquat <hammadprogrammer77>
2024-11-21 19:40:17 +0500

    changes

--- src/components/uplodeImg.js
+++ src/components/uplodeImg.js
@@ -1,6 +1,7 @@
 
 'use client'
-import React, {useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import {useDropzone} from 'react-dropzone';
 
 
