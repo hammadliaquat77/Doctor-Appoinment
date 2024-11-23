b215c0fcb77bfeeb899e0edfea6da20a87e7c79f
Hammad Liaquat <hammadprogrammer77>
2024-11-23 20:09:16 +0500

    changes

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,50 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import ConnectDB from "./lib/connectDB";
+import UserModal from "./lib/models/UserModal";
+
+
+
+ async function handleLogin(obj){
+  await ConnectDB();
+  console.log("connect dtod db")
+  const user = await UserModal.findOne({email : obj.email})
+  console.log("user=>",user)
+  if (user) {
+    return user;
+  }else{
+    let newUser = await UserModal(obj);
+    newUser = await newUser.save();
+    return newUser;
+  }
+ }
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  
+
+  callbacks: {
+    async signIn({ account, profile }) {
+      try {
+        if (account.provider === "google") {
+             console.log('account=>',account);
+             console.log('profile=>',profile);
+           
+           let object = {
+             firstName: profile.given_name,
+             lastName: profile.family_name,
+             email: profile.email,
+             picture: profile.picture,
+           }  
+           const user = await handleLogin(object) 
+        }
+        return true 
+        
+      } catch (error) {
+        console.log('error', error)
+        return false;
+      }
+    },
+  },
+
 })
--- src/lib/models/UserModal.js
+++ src/lib/models/UserModal.js
@@ -2,21 +2,23 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema ({
-    firstName : String,
-    lastName : String,
-    email : String,
-    picture : String,
-    role : {type : String , default: "user" , enum : ["user","doctor","admin"]},
-    extraInfo : {
-        fess : Number,
-        hospital : String,
-        time : String,
-        bio : String,
-        specialization : String,
-        gender : String,
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    picture: String,
+    role: { type: String, default: "user", enum: ["user", "doctor", "admin"] },
+    extraInfo: {
+        fess: Number,
+        hospital: String,
+        time: String,
+        bio: String,
+        specialization: String,
+        gender: String,
     }
 });
 
-export const UserModal = 
-mongoose.models.Users ||  mongoose.model("Users", userSchema);
+const UserModal =
+    mongoose.models?.users || mongoose.model("users", userSchema);
+
+export default UserModal;
